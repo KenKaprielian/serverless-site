b5299a866fa4323c8f0425d2d8cbd792a877420b
KenKaprielian <30817280+KenKaprielian@users.noreply.github.com>
2021-04-22 09:38:03 -0400

    user managment

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-2_gTGeIMEgn', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5ap2nqm99k411hummi44588h7u', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-2', // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'Base URL of your API including the stage', // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod'
     }
 };
