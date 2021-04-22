956528d1d84131e4ac203369d9312bdfd4d31750
KenKaprielian <30817280+KenKaprielian@users.noreply.github.com>
2021-04-22 15:43:31 -0400

    switching to east 1

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-2_gTGeIMEgn', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '5ap2nqm99k411hummi44588h7u', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-east-2', // e.g. us-east-2
+        userPoolId: 'us-east-1_PNMa9V7CI', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5fps07b87iumg8465au5jq1v4e', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1', // e.g. us-east-2
     },
     api: {
         invokeUrl: 'https://sdlqm3vuy7.execute-api.us-east-2.amazonaws.com/prod', // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod'
